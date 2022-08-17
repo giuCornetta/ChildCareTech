c83f4928e6151924a1093823b2264de33910667d
Jacopo Vismara <jacopo.vismara@mail.polimi.it>
2022-08-17 20:08:40 +0200

    minor changes

---
+++ src/main/java/it/polimi/inginf/childcaretech/Repositories/DoctorRepository.java
@@ -0,0 +1,8 @@
+package it.polimi.inginf.childcaretech.Repositories;
+
+import it.polimi.inginf.childcaretech.Data.Doctor;
+import org.springframework.data.repository.CrudRepository;
+
+
+public interface DoctorRepository extends CrudRepository<Doctor, Integer> {
+}
